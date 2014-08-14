d0ad4b5b4d65cba50e14e869ccf91862431c5f17
Cory G Watson <gphat@keen.io>
2014-08-13 17:59:37 -0700

    lol what is close

--- app/models/ZkKafka.scala
+++ app/models/ZkKafka.scala
@@ -41,20 +41,20 @@ object ZkKafka {
     // We assume that there's only one child. This might break things
     val parts = zk.getChildren(s"$stormZkRoot$root/" + s(0))
 
-    return parts.par.map({ vp =>
+    return parts.map({ vp =>
       val jsonState = new String(zk.get(s"$stormZkRoot$root/" + s(0) + s"/$vp"))
       val state = Json.parse(jsonState)
       val offset = (state \ "offset").as[Long]
       val partition = (state \ "partition").as[Long]
       val ttopic = (state \ "topic").as[String]
       (partition.toInt, offset)
-    }).seq.toMap
+    }).toMap
   }
 
   def getKafkaState(topic: String): Map[Int, Long] = {
 
     val kParts = zk.getChildren(s"$kafkaZkRoot/brokers/topics/$topic/partitions")
-    kParts.par.map({ kp =>
+    kParts.map({ kp =>
       val jsonState = new String(zk.get(s"$kafkaZkRoot/brokers/topics/$topic/partitions/$kp/state"))
       val state = Json.parse(jsonState)
       val leader = (state \ "leader").as[Long]
@@ -75,7 +75,8 @@ object ZkKafka {
       if(response.hasError) {
         println("ERROR!")
       }
+      ks.close
       (kp.toInt, response.partitionErrorAndOffsets.get(topicAndPartition).get.offsets(0))
-    }).seq.toMap
+    }).toMap
   }
 }
