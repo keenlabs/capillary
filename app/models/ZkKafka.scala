d970a670e8e7e0bcc7480e59914a34653ebbb323
Jay Shirley <git@shirley.im>
2015-04-07 13:57:00 -0700

    Safer Scalaing

--- app/models/ZkKafka.scala
+++ app/models/ZkKafka.scala
@@ -54,19 +54,21 @@ object ZkKafka {
 
   def getSpoutTopology(root: String): Option[Topology] = {
     // Fetch the spout root
-    val s = zkClient.getChildren.forPath(makePath(applyBase(Seq(stormZkRoot, Some(root)))))
-    // Fetch the partitions so we can pick the first one
-    val parts = zkClient.getChildren.forPath(makePath(applyBase(Seq(stormZkRoot, Some(root))) ++ Seq(Some(s.get(0)))))
-    // Use the first partition's data to build up info about the topology
-    // Also, don't trust zk to have valid JSON, it may be null or something...
-    val path = makePath(applyBase(Seq(stormZkRoot, Some(root))) ++ Seq(Some(s.get(0))) ++ Seq(Some(parts.get(0))))
-    getZkData(path).map { zkData =>
-      val jsonState = new String(zkData)
-      val state = Json.parse(jsonState)
-      val topic = (state \ "topic").as[String]
-      val name = (state \ "topology" \ "name").as[String]
-      Topology(name = name, topic = topic, spoutRoot = root)
-    }
+    zkClient.getChildren.forPath(makePath(applyBase(Seq(stormZkRoot, Some(root))))).asScala.flatMap({ spout =>
+      zkClient.getChildren.forPath(makePath(applyBase(Seq(stormZkRoot, Some(root))) ++ Seq(Some(spout)))).asScala.flatMap({ part =>
+        // Fetch the partitions so we can pick the first one
+        val path = makePath(applyBase(Seq(stormZkRoot, Some(root))) ++ Seq(Some(spout)) ++ Seq(Some(part)))
+        // Use the first partition's data to build up info about the topology
+        // Also, don't trust zk to have valid JSON, it may be null or something...
+        getZkData(path).map { zkData =>
+          val jsonState = new String(zkData)
+          val state = Json.parse(jsonState)
+          val topic = (state \ "topic").as[String]
+          val name  = (state \ "topology" \ "name").as[String]
+          Topology(name = name, topic = topic, spoutRoot = root)
+        }
+      })
+    }).headOption
   }
 
   def getSpoutState(root: String, topic: String): Map[Int, Long] = {
