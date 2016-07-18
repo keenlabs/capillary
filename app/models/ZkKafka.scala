a5d87f0b2b031af7cd9487b308ff02d0a5f8142f
Daniel Kador <dkador@gmail.com>
2016-07-18 15:24:15 +0200

    isDefined check should be isEmpty

--- app/models/ZkKafka.scala
+++ app/models/ZkKafka.scala
@@ -52,7 +52,7 @@ object ZkKafka {
     val maybeData = Option(zkClient.getData.forPath(path))
 
     // log a message if we get a null result for a path read
-    maybeData.map(Option(_)).filter(_.isDefined).foreach(_ => Logger.warn(s"null data retrieved for path $path"))
+    maybeData.map(Option(_)).filter(_.isEmpty).foreach(_ => Logger.warn(s"null data retrieved for path $path"))
 
     if (maybeData.isEmpty) {
       Logger.error("Zookeeper Path " + path + " returned (null)!")
