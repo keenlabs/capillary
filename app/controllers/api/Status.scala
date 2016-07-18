26b30d4be748b654fccaf6e9d05c8ee8b3984326
Daniel Kador <dkador@gmail.com>
2016-07-18 14:45:11 +0200

    cleanup Status.scala

--- app/controllers/api/Status.scala
+++ app/controllers/api/Status.scala
@@ -1,24 +1,18 @@
 package controllers.api
 
+import _root_.utils.JsonFormats._
 import models.ZkKafka
-import models.ZkKafka._
-import play.api._
 import play.api.libs.json._
 import play.api.mvc._
-import _root_.utils.JsonFormats._
 
 object Status extends Controller {
 
-  def current(topoRoot: String, topic: String) = Action {
-
-    val stormState = ZkKafka.getSpoutState(topoRoot, topic)
-
-    val zkState = ZkKafka.getKafkaState(topic)
+  def current(topoRoot: String, topic: String): Action[AnyContent] = Action {
 
     val totalsAndDeltas = ZkKafka.getTopologyDeltas(topoRoot, topic)
 
     Ok(Json.toJson(
-      totalsAndDeltas._2.toSeq
+      totalsAndDeltas._2
     ))
   }
 }
